9888ab67ed0cedb0b5968e689b9d299e220fd3ed
glitcher93 <nigeljdsouza@gmail.com>
2022-03-24 18:36:59 -0400

    created getAllInventoryItems funcion to get all inventory items

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -2,3 +2,8 @@ const readInventory = require("../utils/readWrite").readInventory;
 const writeInventory = require("../utils/readWrite").writeInvereadInventory;
 const readWarehouses = require("../utils/readWrite").readWarehouses;
 const { v4: uuidv4 } = require("uuid");
+
+const getAllInventoryItems = (req, res) => {
+    const inventoryData = readInventory();
+    res.status(200).json(inventoryData);
+}
