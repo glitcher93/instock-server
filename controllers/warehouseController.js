0c4770d38a4200206a3c00b87a1a2fe575bb6380
glitcher93 <nigeljdsouza@gmail.com>
2022-03-24 17:27:16 -0400

    created getSingleWarehouse function to retrieve info for a single warehouse

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -6,3 +6,13 @@ const getAllWarehouses = (req, res) => {
     const warehouseData = readWarehouses();
     res.status(200).json(warehouseData);
 }
+
+const getSingleWarehouse = (req, res) => {
+    const warehouseData = readWarehouses();
+    const warehouseId = req.params.id;
+    const foundWarehouse = warehouseData.find(warehouse => warehouse.id === warehouseId)
+    if (!foundWarehouse) {
+        res.status(404).send("Warehouse not found")
+    }
+    res.status(200).json(foundWarehouse)
+}
