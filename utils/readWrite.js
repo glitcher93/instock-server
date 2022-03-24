dedabfa2dd1cfc8c9545eadaa9d098cb9c1c9eb2
glitcher93 <nigeljdsouza@gmail.com>
2022-03-24 15:49:14 -0400

    created readWrite file to house read and write functions

---
+++ utils/readWrite.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const readWarehouses = () => {
+    const warehouseData = fs.readFileSync("./data/warehouses.json");
+    const parsedData = JSON.parse(warehouseData);
+    return parsedData;
+}
+
+const readInventory = () => {
+    const warehouseData = fs.readFileSync("./data/inventories.json");
+    const parsedData = JSON.parse(warehouseData);
+    return parsedData;
+}
+
+const writeWarehouses = (data) => {
+    const stringifiedData = JSON.stringify(data);
+    fs.writeFile('./data/inventories.json', stringifiedData)
+}
+
+const writeInventory = (data) => {
+    const stringifiedData = JSON.stringify(data);
+    fs.writeFile('./data/inventories.json', stringifiedData)
+}
+
+module.exports = {
+    readWarehouses,
+    readInventory,
+    writeWarehouses,
+    writeInventory
+}
