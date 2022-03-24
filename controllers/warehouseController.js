5353540284fbf513e837b7b34b7ff653ea7501b2
glitcher93 <nigeljdsouza@gmail.com>
2022-03-24 17:28:39 -0400

    created postNewWarehouse function to add a new warehouse

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -16,3 +16,31 @@ const getSingleWarehouse = (req, res) => {
     }
     res.status(200).json(foundWarehouse)
 }
+
+const postNewWarehouse = (req, res) => {
+    const { name, address, city, country, contact } = req.body;
+    const emailRegEx = /\S+@\S+\.\S+/;
+    const phoneRegEx = /^[+]?[1]?[\s]?[(]?[0-9]{3}[)]?[\s]?[0-9]{3}[-]?[0-9]{4}$/;
+
+    if (!contact.phone.match(phoneRegEx) && !contact.email.match(emailRegEx)) {
+        res.status(400).send("Phone and/or email address not formatted correctly")
+    } else {
+        const warehouseData = readWarehouses();
+        const newWarehouse = {
+            id: uuidv4(),
+            name,
+            address,
+            city,
+            country,
+            contact: {
+                name: contact.name,
+                position: contact.position,
+                phone: contact.phone,
+                email: contact.email
+            }
+        };
+        warehouseData.push(newWarehouse);
+        writeWarehouses(warehouseData);
+        res.status(201).json(newWarehouse);
+    }
+}
