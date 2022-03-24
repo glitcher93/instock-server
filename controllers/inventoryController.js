12ebadeda01a1a9855c425efca6ec1966b6e1ca7
glitcher93 <nigeljdsouza@gmail.com>
2022-03-24 16:32:43 -0400

    initial setup of controller files

---
+++ controllers/inventoryController.js
@@ -0,0 +1,4 @@
+const readInventory = require("../utils/readWrite").readInventory;
+const writeInventory = require("../utils/readWrite").writeInvereadInventory;
+const readWarehouses = require("../utils/readWrite").readWarehouses;
+const { v4: uuidv4 } = require("uuid");
---
+++ controllers/warehouseController.js
@@ -0,0 +1,3 @@
+const readWarehouses = require("../utils/readWrite").readWarehouses;
+const writeWarehouses = require("../utils/readWrite").writeWarehouses;
+const { v4: uuidv4 } = require("uuid");
