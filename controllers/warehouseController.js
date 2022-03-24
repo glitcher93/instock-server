b605a386b17c1686fe73c4224ccee325b6fac941
glitcher93 <nigeljdsouza@gmail.com>
2022-03-24 17:25:39 -0400

    created getAllWarehouses function to retrieve info of all warehouses

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,3 +1,8 @@
 const readWarehouses = require("../utils/readWrite").readWarehouses;
 const writeWarehouses = require("../utils/readWrite").writeWarehouses;
 const { v4: uuidv4 } = require("uuid");
+
+const getAllWarehouses = (req, res) => {
+    const warehouseData = readWarehouses();
+    res.status(200).json(warehouseData);
+}
