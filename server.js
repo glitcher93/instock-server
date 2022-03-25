4b77beeb90477c9a9d8a0a4b814315f2468c1bea
glitcher93 <nigeljdsouza@gmail.com>
2022-03-25 10:17:23 -0400

    fixed line 10 so that it invokes cors, changed paths on lines 6 and 7 to go to routes instead of controllers

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 7400;
-const warehouseRoutes = require('./controllers/warehouses');
-const inventoryRoutes = require('./controllers/inventories')
+const warehouseRoutes = require('./routes/warehouses');
+const inventoryRoutes = require('./routes/inventories')
 
 app.use(express.json());
-app.use(cors)
+app.use(cors())
 app.use('/warehouses', warehouseRoutes);
 app.use('/inventory', inventoryRoutes)
 
