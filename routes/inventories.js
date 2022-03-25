3e3b454f63299f6c90b4e8393b15b594830d6d8d
glitcher93 <nigeljdsouza@gmail.com>
2022-03-25 10:19:59 -0400

    added getInventoryForSingleWarehouse import, added new get route for the previous function, exported router

--- routes/inventories.js
+++ routes/inventories.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const inventoryController = require('../controllers/inventoryController');
-const { getAllInventoryItems, getSingleInventoryItem, postNewInventoryItem, updateInventoryItem, deleteInventoryItem  } = inventoryController;
+const { getAllInventoryItems, getSingleInventoryItem, getInventoryForSingleWarehouse, postNewInventoryItem, updateInventoryItem, deleteInventoryItem } = inventoryController;
 
 router
     .route('/')
@@ -12,9 +12,12 @@ router
     .get('/:id', getSingleInventoryItem)
 
 router
+    .get('/warehouse/:warehouseId', getInventoryForSingleWarehouse)
+
+router
     .patch('/edit/:id', updateInventoryItem)
 
 router
     .delete('/delete/:id', deleteInventoryItem)
 
-
+module.exports = router;
