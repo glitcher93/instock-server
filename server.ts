1336fa1664fffaff568043f40755a936a1d8b25b
glitcher93 <nigeljdsouza@gmail.com>
2023-07-25 22:03:50 -0400

    integrated jsonServer

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import jsonServer from 'json-server';
+import path from 'path';
 import warehouseRoutes from './routes/warehouses';
 import inventoryRoutes from './routes/inventories';
 
@@ -12,8 +14,10 @@ const app: Express = express();
 
 
 app.use(express.json());
-app.use(cors())
+app.use(jsonServer.router(path.join(__dirname, 'data/inventories.json')));
+app.use(jsonServer.router(path.join(__dirname, 'data/warehouses.json')));
+app.use(cors());
 app.use('/warehouses', warehouseRoutes);
-app.use('/inventory', inventoryRoutes)
+app.use('/inventory', inventoryRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
